81ae4de4e116cff2529c73f1ad2649b20fbd00a1
lmccray <lmccray@uhin.org>
2019-01-09 15:51:48 -0700

    Got concert-this working

--- keys.js
+++ keys.js
@@ -1,8 +1,11 @@
 var dotenv = require("dotenv").config();
-console.log('this is loaded');
+//console.log('this is loaded');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
 
+exports.omdb = {
+    key: process.env.OMDB_KEY
+};
--- liri.js
+++ liri.js
@@ -1,7 +1,44 @@
 var keys = require("./keys");
 var request = require('request');
 var Spotify = require('node-spotify-api');
+var moment = require('moment');
+
+function concertThis(artist) {
+    var inputartist = artist
+
+    var options = {
+        method: 'GET',
+        url: 'https://rest.bandsintown.com/artists/' + inputartist + '/events',
+        qs: { app_id: 'codingbootcamp' },
+
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log('Name of the venue: ' + JSON.parse(body)[0].venue.name);
+        console.log('Venue location: ' + JSON.parse(body)[0].venue.city + ', ' + JSON.parse(body)[0].venue.region);
+        console.log('Date of event: ' + moment(JSON.parse(body)[0].datetime).format('MM/DD/YYYY'));
+
+    });
+
+}
+
+switch (process.argv[2]) {
+    case 'concert-this':
+        concertThis(process.argv[3])
+        break;
+    case 'spotify-this-song':
+        break;
+    case 'movie-this':
+        break;
+    case 'do-what-it-says':
+        break;
+    default:
+
+}
 
 
-console.log(process.argv[2]);
-console.log(keys.spotify);
+
+//console.log(process.argv[2]);
+//console.log(keys.spotify);
